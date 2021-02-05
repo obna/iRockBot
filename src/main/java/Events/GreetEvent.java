b187ec722220bf1b46904caf2bf425a82d6b3a6c
OBNA <obna.adeleke@gmail.com>
2021-02-05 17:52:43 -0600

    Initial commit

---
+++ src/main/java/Events/GreetEvent.java
@@ -0,0 +1,14 @@
+package Events;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class GreetEvent extends ListenerAdapter {
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String messageSent = event.getMessage().getContentRaw();
+        if (messageSent.equalsIgnoreCase( "hello")){
+            event.getChannel().sendMessage("Hi").queue();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        JDA jda = new JDABuilder("ODA2ODM5NzIyNjU5MTUxODcz.YBvSDA.gLoyXOcF0IS43aECzU32NSMv660").build();
+
+
+    }
+}
