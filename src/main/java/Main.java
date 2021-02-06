5cdf3a8fb3e42cf52f194cd1ce9916a729735a6e
OBNA <obna.adeleke@gmail.com>
2021-02-05 18:22:52 -0600

    Add irockbot-key

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+    private static final String IROCKBOT_KEY = "irockbot-token.txt";
+
+    public static void main(String[] args) throws Exception {
+        JDA jda = new JDABuilder(IROCKBOT_KEY).build();
+    }
+}
